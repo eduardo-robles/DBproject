fd3595ac267829e4b6323a12e55679b377f62f2d
Eduardo Robles <eduardorobles@rgvlibrobox.com>
2015-03-22 11:32:56 -0500

    Added tables Contact and Visits

--- phonerec.sql
+++ phonerec.sql
@@ -19,8 +19,8 @@ title varchar(15) NOT NULL,
 email varchar(30) NOT NULL,
 phone varchar(14) NOT NULL,
 address varchar(50) NOT NULL,
-DivisionId int,
-customerId int,
+DivisionId int, -- Foreign Key
+customerId int, -- Foreign Key
 PRIMARY KEY(employeeId)
 )ENGINE=InnoDB;
 
@@ -30,8 +30,8 @@ customerId int(11) NOT NULL AUTO_INCREMENT,
 Name varchar(50),
 phone varchar(14) NOT NULL,
 address varchar(50) NOT NULL,
-employeeId int,
-DivisionId int,
+employeeId int, -- Foreign Key
+DivisionId int, -- Foreign Key
 PRIMARY KEY(customerId)
 )ENGINE=InnoDB;
 
@@ -41,7 +41,7 @@ ContactId int(11) NOT NULL AUTO_INCREMENT,
 fName varchar(25) NOT NULL,
 lName varchar(25) NOT NULL,
 phone varchar(14) NOT NULL,
-employeeId int,
+employeeId int, -- Foreign Key
 PRIMARY KEY(ContactId)
 )ENGINE=InnoDB;
 
@@ -49,48 +49,29 @@ PRIMARY KEY(ContactId)
 CREATE TABLE Division(
 DivisionId int(11) NOT NULL AUTO_INCREMENT,
 address varchar(50) NOT NULL,
-customerId int,
-employeeId int,
+customerId int, -- Foreign Key
+employeeId int, -- Foreign Key
 PRIMARY KEY(DivisionId)
 )ENGINE=InnoDB;
 
--- **************************************** 
--- Alter tables to add FOREIGN KEYS
--- **EXAMPLE ON HOW TO ALTER TABLE
--- ALTER TABLE tableNm
--- ADD FOREIGN KEY () REFERENCES tableNm();
+-- Create table Contacts
+CREATE TABLE Contacts(
+startTime TIME,
+endTime TIME,
+priceRate int(10) NOT NULL
+)ENGINE=InnoDB;
 
--- Add Foreign keys DivisionId and customerId on Employee table
-ALTER TABLE Employee
-ADD FOREIGN KEY (DivisionId) REFERENCES Division(DivisionId);
-
-ALTER TABLE Employee
-ADD FOREIGN KEY (customerId) REFERENCES Customer(customerId);
-
--- Add Foreign keys employeeId and DivisionId on Customer table
-ALTER TABLE Customer
-ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId);
-
-ALTER TABLE Customer
-ADD FOREIGN KEY (DivisionId) REFERENCES Division(DivisionId);
-
--- Add Foreign keys employeeId on PersonalContact table
-ALTER TABLE PersonalContact
-ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId);
-
--- Add Foreign keys customerId and employeeId on Division table
-ALTER TABLE Division
-ADD FOREIGN KEY (customerId) REFERENCES Customer(customerId);
-
-ALTER TABLE Division
-ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId);
+-- Create table Visits
+CREATE TABLE Visits(
+departureDate DATE,
+returnDate DATE
+)ENGINE=InnoDB;
 
 -- ***********************************
 -- Inserting data
 -- **EXAMPLE HOW TO INSERT DATA
 -- INSERT INTO table_name (col1, col2)
 -- VALUES('val1', 'val2');
-
 
 INSERT INTO Employee (fName, lName, title, email, phone, address)
 VALUES ('Grace', 'Hopper', 'General', 'gh@example.com', '956-123-4567', '123 Any Street. Somewhere, USA');
@@ -103,3 +84,42 @@ VALUES('Ada', 'Lovelace', '012-345-6789');
 
 INSERT INTO Division (address)
 VALUES('321 Some Street, Anywhere, USA');
+
+
+-- **************************************** 
+-- Alter tables to add FOREIGN KEYS
+-- **EXAMPLE ON HOW TO ALTER TABLE
+-- ALTER TABLE tableNm
+-- ADD FOREIGN KEY () REFERENCES tableNm();
+
+-- Add Foreign keys DivisionId and customerId on Employee table
+ALTER TABLE Employee
+ADD FOREIGN KEY (DivisionId) REFERENCES Division(DivisionId)
+ON UPDATE CASCADE;
+
+ALTER TABLE Employee
+ADD FOREIGN KEY (customerId) REFERENCES Customer(customerId)
+ON UPDATE CASCADE;
+
+-- Add Foreign keys employeeId and DivisionId on Customer table
+ALTER TABLE Customer
+ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId)
+ON UPDATE CASCADE;
+
+ALTER TABLE Customer
+ADD FOREIGN KEY (DivisionId) REFERENCES Division(DivisionId)
+ON UPDATE CASCADE;
+
+-- Add Foreign keys employeeId on PersonalContact table
+ALTER TABLE PersonalContact
+ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId)
+ON UPDATE CASCADE;
+
+-- Add Foreign keys customerId and employeeId on Division table
+ALTER TABLE Division
+ADD FOREIGN KEY (customerId) REFERENCES Customer(customerId)
+ON UPDATE CASCADE;
+
+ALTER TABLE Division
+ADD FOREIGN KEY (employeeId) REFERENCES Employee(EmployeeId)
+ON UPDATE CASCADE;
